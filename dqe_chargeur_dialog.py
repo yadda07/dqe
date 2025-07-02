a12cae586de2042549e5332a96d2af659573f54f
yadda07 <youcef.geodesien@gmail.com>
2025-07-02 12:51:23 +0200

    'first'

--- dqe_chargeur_dialog.py
+++ dqe_chargeur_dialog.py
@@ -1284,7 +1284,7 @@ class DQEChargeur(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
         
-        self.setWindowTitle("Chargeur DQE - FULL VERSION")
+        self.setWindowTitle("Chargeur DQE")
         self.setMinimumSize(800, 600)
         self.setModal(False)
         
